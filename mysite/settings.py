16f4f12ca1267313ac25b18f633f1ad78e0f6bc5
Guanqing <haoguanqing@gmail.com>
2017-02-21 01:33:31 -0500

    add heroku as allowed host in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pj(c3o_uq@a-jr30o6v2-=b9xj409$bm-oqeqqp!_vokkuk6g8'
 DEBUG = True
 APPEND_SLASH = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tinyurl-hgq.herokuapp.com','localhost']
 
 
 # Application definition
