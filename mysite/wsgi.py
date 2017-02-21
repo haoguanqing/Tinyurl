2cf0e156e324db7d1a084cb25a4fcf1926948d8e
Guanqing <haoguanqing@gmail.com>
2017-02-21 01:41:35 -0500

    add allowed hosts in wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
+
+ALLOWED_HOSTS = ['tinyurl-hgq.herokuapp.com','localhost']
